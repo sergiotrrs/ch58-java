8e45bb470cdddbb4a9f8f5f8aac3b2370fe01fc0
Sergio Torres <sergio.torres@idr.edu.mx>
2025-10-17 13:39:28 -0600

    feat: endpoint findUserById

--- spring03_spring_monkey_tower/src/main/java/com/monkey/tower/app/controller/UserController.java
+++ spring03_spring_monkey_tower/src/main/java/com/monkey/tower/app/controller/UserController.java
@@ -1,5 +1,41 @@
 package com.monkey.tower.app.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.monkey.tower.app.model.User;
+import com.monkey.tower.app.service.UserService;
+
+
+@RestController
+@RequestMapping("/api/v1/users")
+@CrossOrigin(origins = "*")
 public class UserController {
+	
+	private final UserService userService;
+
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@PostMapping
+	ResponseEntity<User> createdUser(@RequestBody User user){
+		User createdUser = userService.save(user);
+		return ResponseEntity.status(201).body(createdUser);
+	}
+	
+	@GetMapping("/{id}")
+	ResponseEntity<User> findById(@PathVariable("id") Long id){
+		User existingUser = userService.findById(id);
+		return ResponseEntity.ok(existingUser);
+	}
+	
+	
 
 }
--- spring03_spring_monkey_tower/src/main/java/com/monkey/tower/app/service/impl/UserServiceImpl.java
+++ spring03_spring_monkey_tower/src/main/java/com/monkey/tower/app/service/impl/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.monkey.tower.app.service.impl;
 
+import java.util.Optional;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.monkey.tower.app.model.User;
@@ -10,22 +13,30 @@ import com.monkey.tower.app.service.UserService;
 public class UserServiceImpl implements UserService {
 
 	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
 		
-	public UserServiceImpl(UserRepository userRepository) {
+	public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
 		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Override
 	public User save(User user) {
 		// TODO verificar si el usuario existe
 		// Si existe lanzar una excepcion
+		user.setPassword(passwordEncoder.encode( user.getPassword()) );
+		System.out.println(user);
 		return userRepository.save(user);
 	}
 
 	@Override
 	public User findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<User> optionalUser = userRepository.findById(id);
+		if( optionalUser.isEmpty()) {
+			throw new IllegalStateException("User doesnot exist with id " + id);			
+		}
+		User existingUser = optionalUser.get();
+		return existingUser;
 	}
 
 	@Override
